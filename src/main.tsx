88bfa692dad1d9e30e6a2d4957358af6a68df67a
GbemiDaniel <gbemidaniel01@gmail.com>
2026-02-17 15:37:07 +0100

    Removed framer traansition to improve ux

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import "./styles/tokens.css";
 import "./styles/globals.css";
+import { MotionConfig } from "framer-motion";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <App />
+      <MotionConfig transition={{ duration: 0 }}>
+        <App />
+      </MotionConfig>{" "}
     </React.StrictMode>,
   );
 }
